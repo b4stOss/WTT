da7e046aa09bc9a183b9bc84aeab176a8ae6c750
batgame <batgen42@gmail.com>
2021-10-20 21:18:20 +0200

    Change for asyncronous requests

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import json
+import asyncio
+from os import pipe
 import requests
 import pprint
-
+import aiohttp
 from requests.models import Response
 from requests.sessions import PreparedRequest
 
@@ -23,22 +25,23 @@ class Crypto:
         self.name = name 
         self.limit = limit
 
-        self.get_periods()
+    async def get_periods(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://min-api.cryptocompare.com/data/v2/histoday?fsym={self.name}&tsym=USD&limit={self.limit}&api_key=4a6601d2ebd6b909fdd91bbfc7f45016438a536204390ee6bbb01f30e2cee65b") as response:
+                json = await response.json()
+                if json["Response"] == "Success":
+                    data = json["Data"]["Data"]
 
-    def get_periods(self):
-        response = requests.get(f"https://min-api.cryptocompare.com/data/v2/histoday?fsym={self.name}&tsym=USD&limit={self.limit}").json()
-
-        if response["Response"] == "Success":
-            data = response["Data"]["Data"]
-
-            for period in data:
-                self.price_list.append(period["close"])
+                    for period in data:
+                        self.price_list.append(period["close"])
+                else:
+                    print(json["Message"])
     
     @property
     def mooving_average(self):
         if len(self.price_list) != 0:
             return sum(self.price_list) / len(self.price_list)
-        
+
         return "NaN"
         
 
@@ -49,14 +52,23 @@ class Crypto:
     def __str__(self):
         return f"{self.id} - {self.name} - {self.mooving_average}"
 
-def get_top_100_cryptos():
+
+async def crypto_async(info):
+    crypto = Crypto.create_with_info(info)
+    await crypto.get_periods()
+    await asyncio.sleep(1)
+    return crypto
+
+async def get_top_100_cryptos():
+    tasks = []
     response = requests.get("https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD")
     response = response.json()["Data"]
 
     for crypto in response:
-        info = crypto["CoinInfo"]
-        yield Crypto.create_with_info(info)
+        info = crypto["CoinInfo"]        
+        tasks.append(crypto_async(info))
+        
+    r = await asyncio.gather(*tasks)
+    #[print(x) for x in r]
 
-
-for crypto in get_top_100_cryptos():
-    print(crypto)       
+asyncio.run(get_top_100_cryptos())
